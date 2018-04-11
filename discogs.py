4ee3f72b0afe3a42045fb90138226e0402747283
Walt <wpniederer@cpp.edu>
2018-04-11 01:48:13 -0700

    added conditions, v1.0

--- discogs.py
+++ discogs.py
@@ -6,37 +6,45 @@ from itertools import islice
 user_agent = ('PriceChecker/0.1' '+https://github.com/wpniederer/PriceChecker')
 utoken = config.user_token
 discogsclient = discogs_client.Client(user_agent, user_token=utoken)
+num_to_print = 30
 
-#user_input = input("Enter artist and album separated by '|' (artist|album): ").split('|')
+user_input = input("Enter artist and album separated by '|' (artist|album) or just artist to find all albums: ").split('|')
 
-user_input = ['The Strokes','Is This It']
-search_results = discogsclient.search(user_input[1], type='release',
+if (len(user_input) == 1):
+    # user_input = 'The Strokes'
+    user_input = user_input[0]
+    search_results = discogsclient.search(artist=user_input, type='master', format='album')
+
+    print('\n{:=^100}'.format('Albums by ' + user_input))
+    print('{:^20s}|{:^20s}|'.format('Discogs ID', 'Album'))
+    print('{:-^100}'.format('-'))
+
+    for release in islice(search_results, num_to_print):
+        print('{id:^20}|'.format(id=release.id), end='')
+        print('{album:20}|'.format(album=release.title))
+    print('{:-^100}'.format('-'))
+
+elif (len(user_input) == 2):
+    # user_input = ['The Strokes','Is This It']
+    search_results = discogsclient.search(user_input[1], type='release',
         artist=user_input[0], format='album, LP')
 
-num_to_print = 5
-print('\n{:=^100}'.format('Releases of ' + user_input[1] + ' by ' + user_input[0]))
-print('{:^20s}|{:^20s}|{:^20s}|{:^20s}|{:^20s}'.format('Discogs ID', 'Artist', 'Album', 'Year', 'Label'))
-print('{:-^100}'.format('-'))
-for release in islice(search_results, num_to_print):
-    print('{id:^20}|'.format(id=release.id), end='')
-    print('{artist:20}|'.format(artist=', '.join(artist.name for artist in release.artists)), end='')
-    print('{album:20}|'.format(album=release.title), end='')
-    if (release.year == 0):
-        print('{message:^20}|'.format(message='Year not given'), end='')
-    else:
-        print('{year:^20}|'.format(year=release.year), end='')
-    print('{label:20}'.format(label=', '.join(label.name for label in
-                                        release.labels)))
-print('{:-^100}'.format('-'))
+    print('\n{:=^100}'.format('Releases of ' + user_input[1] + ' by ' + user_input[0]))
+    print('{:^20s}|{:^20s}|{:^20s}|{:^20s}|{:^20s}'.format('Discogs ID', 'Artist', 'Album', 'Year', 'Label'))
+    print('{:-^100}'.format('-'))
 
-user_input = 'The Strokes'
-#artist = discogsclient.artist(user_input)
-search_results = discogsclient.search(artist=user_input, type='master', format='album')
-print('\n{:=^100}'.format('Albums by ' + user_input))
-print('{:^20s}|{:^20s}|'.format('Discogs ID', 'Album'))
-print('{:-^100}'.format('-'))
-for release in islice(search_results, num_to_print):
-    print('{id:^20}|'.format(id=release.id), end='')
-    print('{album:20}|'.format(album=release.title))
-print('{:-^100}'.format('-'))
+    for release in islice(search_results, num_to_print):
+        print('{id:^20}|'.format(id=release.id), end='')
+        print('{artist:20}|'.format(artist=', '.join(artist.name for artist in release.artists)), end='')
+        print('{album:20}|'.format(album=release.title), end='')
+        if (release.year == 0):
+            print('{message:^20}|'.format(message='Year not given'), end='')
+        else:
+            print('{year:^20}|'.format(year=release.year), end='')
+        print('{label:20}'.format(label=', '.join(label.name for label in
+                                        release.labels)))
+    print('{:-^100}'.format('-'))
+else:
+    print('\nInvalid Input')
+
 
