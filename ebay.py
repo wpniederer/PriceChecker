b5bb18f2be7a944ed6ff961bd9ca0508fd0ea125
Walt <wpniederer@cpp.edu>
2018-04-12 18:33:48 -0700

    testing finding api

--- ebay.py
+++ ebay.py
@@ -1,8 +1,35 @@
 import datetime
-import ebaysdk
 import config
+from ebaysdk.finding import Connection as Finding
+
 
 app_id = config.app_id
+artist = 'The Strokes'
+max_price = '20'
+min_price = '5'
+condition = 'New'
+num_to_print = '25'
 
-ebay_client = ebaysdk.finding(appid=app_id, config_file=None)
-response = ebay_client.execute('findItemsAdvanced', {'keywords': 'Python'})
+
+ebay_client = Finding(siteid='EBAY-US', appid=app_id, config_file=None)
+ebay_client.execute('findItemsAdvanced', {
+    'keywords': artist,
+    'categoryId': 176985,
+    'itemFilter': [
+        {'name': 'MaxPrice', 'value': max_price, 'paramName': 'Currency', 'paramValue': 'USD'},
+        {'name': 'MinPrice', 'value': min_price, 'paramName': 'Currency', 'paramValue': 'USD'},
+        {'name': 'Condition', 'value': condition},
+    ],
+    'paginationInput': {
+        'entriesPerPage': num_to_print,
+        'pageNumber': '1'
+    },
+    'sortOrder': 'CurrentPriceHighest'
+})
+
+search = ebay_client.response.dict()
+
+for item in search['searchResult']['item']:
+    print('ItemID: {}'.format(item['itemId']))
+    print('Title: {}'.format(item['title']))
+
