0577d428da5d4af96d69b7b512efa1cadf036c43
Walt <wpniederer@cpp.edu>
2018-04-16 02:16:46 -0700

    cleaning up code, added conditions for no results

--- discogs.py
+++ discogs.py
@@ -1,5 +1,4 @@
 # consider giving user to search for all releases vs just albums/masters
-# catch ratelimit error, pause, and continue search after 60 secs
 import discogs_client
 from discogs_client import exceptions
 from itertools import islice
@@ -9,81 +8,72 @@ import time
 user_agent = ('PriceChecker/1.0' '+https://github.com/wpniederer/PriceChecker')
 utoken = config.discogs_user_token
 discogsclient = discogs_client.Client(user_agent, user_token=utoken)
-num_to_print = 155
+num_to_print = 200
 sleep_time = 2
 user_input = input("Enter artist and album separated by '|' (artist|album) or just artist to find all albums: ").split('|')
 
 def urlbuilder(discogs_id, r_type):
-    #translator = str.maketrans('', '', string.punctuation)
-    #artist_name = artist_name.translate(translator)
     url = 'https://www.discogs.com/{type:}/{id:}'.format(type=r_type, id=discogs_id)
-    #url = url.replace(' ', '-')
     return url
 
-
+# Artist album search
 if (len(user_input) == 1):
     # user_input = 'The Strokes'
     user_input = user_input[0]
-    search_results = discogsclient.search(artist=user_input, type='master', format='album',
-                                          sort='title', sort_order='asc')
     release_type = 'master'
 
-    #if (len(search_results) == 0 ):
-        #print('\nCould not find any albums released by ' + user_input + ',searching for all releases(EPs/Singles/Compilations/etc....)')
-        #time.sleep(sleep_time)
-        #search_results = discogsclient.search(artist=user_input, type='release', sort='title', sort_order='asc')
-        #release_type = 'release'
+    # album search
+    search_results = discogsclient.search(artist=user_input, type='master', format='album',
+                                          sort='title', sort_order='asc')
 
-    print('\n{:=^135}'.format('Albums by ' + user_input))
-    print('{:^90}|{:^45}'.format('Album', 'Link'))
-    print('{:-^135}'.format('-'))
+    if (len(search_results) > 0):
+        print('\n{:=^135}'.format('Albums by ' + user_input))
+        print('{:^90}|{:^45}'.format('Album', 'Link'))
+        print('{:-^135}'.format('-'))
 
-    for release in islice(search_results, num_to_print):
-        #print('{id:^20}|'.format(id=release.id), end='')
-        print('{album:90}|'.format(album=release.title), end='')
-        print('{url:45}'.format(url=urlbuilder(release.id, release_type)))
-    print('{:-^135}'.format('-'))
+        for release in islice(search_results, num_to_print):
+            print('{album:90}|'.format(album=release.title), end='')
+            print('{url:45}'.format(url=urlbuilder(release.id, release_type)))
+        print('{:-^135}'.format('-'))
 
+    # EP search
     search_results = discogsclient.search(artist=user_input, type='master', format='EP',
                                           sort='title', sort_order='asc')
 
-    print('\n{:=^135}'.format('EPs by ' + user_input))
-    print('{:^90}|{:^45}'.format('EP', 'Link'))
-    print('{:-^135}'.format('-'))
+    if (len(search_results) > 0):
+        print('\n{:=^135}'.format('EPs by ' + user_input))
+        print('{:^90}|{:^45}'.format('EP', 'Link'))
+        print('{:-^135}'.format('-'))
 
-    for release in islice(search_results, num_to_print):
-        #print('{id:^20}|'.format(id=release.id), end='')
-        print('{album:90}|'.format(album=release.title), end='')
-        print('{url:45}'.format(url=urlbuilder(release.id, release_type)))
-    print('{:-^135}'.format('-'))
+        for release in islice(search_results, num_to_print):
+            print('{album:90}|'.format(album=release.title), end='')
+            print('{url:45}'.format(url=urlbuilder(release.id, release_type)))
+        print('{:-^135}'.format('-'))
 
+# record release search
 elif (len(user_input) == 2):
     # user_input = ['The Strokes','Is This It']
+    release_type = 'release'
     search_results = discogsclient.search(title=user_input[1], type='release',
         artist=user_input[0], format='vinyl', sort='year', sort_order='asc')
 
-    if (len(search_results) > 60):
+    if (len(search_results) == 0):
+        print('No results for {} by {}.'.format(user_input[0], user_input[1]))
+        quit()
+
+    elif (len(search_results) > 60):
         print('\n{} by {} has over {} releases, mostly likely due to multiple international releases....limiting search to US'
                 .format(user_input[1], user_input[0], len(search_results)))
         time.sleep(sleep_time)
         search_results = discogsclient.search(title=user_input[1], type='release',
                                           artist=user_input[0], format='vinyl', sort='year', sort_order='asc', country='us')
 
-    #if (len(search_results) == 0):
-        #print('\n{} by {} has no releases under the album tag, retrying as a general search of records'.format(user_input[1], user_input[0]))
-        #time.sleep(sleep_time)
-        #search_results = discogsclient.search(title=user_input[1], type='release',
-                                          #artist=user_input[0], format='vinyl', sort='year', sort_order='asc')
-
-    release_type = 'release'
     print('\n{:=^120}'.format('Releases of ' + user_input[1] + ' by ' + user_input[0]))
     print('{:^20s}|{:^20s}|{:^14s}|{:^9s}|{:^45s}'
           .format('Artist', 'Album', 'Year', 'Country', 'Link'))
     print('{:-^120}'.format('-'))
 
-    count = 0
     for release in islice(search_results, num_to_print):
-            #print('{id:^20}|'.format(id=release.id), end='')
         try:
             print('{artist:20}|'.format(artist=', '.join(artist.name for artist in release.artists)), end='')
             print('{album:20}|'.format(album=release.title), end='')
@@ -94,12 +84,20 @@ elif (len(user_input) == 2):
             print('{country:^9}|'.format(country=release.country),end='')
             print('{url:45}|'.format(url=urlbuilder(release.id, release_type)))
 
-        except (exceptions.HTTPError):
+        except(exceptions.HTTPError):
             user_input2 = input("\nMaxed out number of requests that can be made in a minute...(w)ait or (e)xit: ")
+            #user_input2 = 'w'
+
             if (user_input2 == 'w'):
-                print('\nWaiting 60 seconds\n')
-                time.sleep(60)
+                print('\nWaiting 61 seconds\n')
+                time.sleep(61)
+
+                print('\n{:=^120}'.format('Releases of ' + user_input[1] + ' by ' + user_input[0]))
+                print('{:^20s}|{:^20s}|{:^14s}|{:^9s}|{:^45s}'
+                    .format('Artist', 'Album', 'Year', 'Country', 'Link'))
+                print('{:-^120}'.format('-'))
                 continue
+
             else:
                 print('exiting....')
                 time.sleep(2)
@@ -109,5 +107,3 @@ elif (len(user_input) == 2):
 
 else:
     print('\nInvalid Input')
-
-
