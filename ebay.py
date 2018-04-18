b87fb9a04fd5983d9369c8b507b80a20ce6b0ee6
Walt <wpniederer@cpp.edu>
2018-04-18 01:48:24 -0700

    changing output formatting

--- ebay.py
+++ ebay.py
@@ -2,6 +2,7 @@
 # listing title -- price -- url
 import datetime
 import config
+import time
 from ebaysdk.finding import Connection as Finding
 
 
@@ -10,7 +11,7 @@ search = 'The Strokes'
 max_price = '20'
 min_price = '5'
 condition = 'New'
-num_to_print = '5'
+num_to_print = '50'
 
 
 try:
@@ -35,9 +36,20 @@ except ConnectionError as e:
     print(e)
 
 search_results = ebay_client.response.dict()
+###Need to figure out how to prevent searches with no results#####
+#if(len(search_results['searchResult']) == 0):
+#if(search_results['searchResult']):
+    #print('\nNo search results for {}, now exiting...'.format(search))
+    #time.sleep(2)
+    #quit()
+
+#else:
+print('\n{:=^170}'.format('Search results for ' + search))
+#print('{:^8}|{:^70}|{:^90}'.format('Price', 'Title','URL'))
+#print('{:=^170}'.format('='))
 
 for item in search_results['searchResult']['item']:
-    #print('ItemID: {}'.format(item['itemId']))
-    print('Title: {}'.format(item['title']))
-    print('Price: ${}'.format(item['sellingStatus']['currentPrice']['value']))
-    print('URL: {}'.format(item['viewItemURL']))
+    print('Title: {:70}'.format(item['title']))
+    print('Price: ${:8}'.format(item['sellingStatus']['currentPrice']['value']))
+    print('URL: {:90}'.format(item['viewItemURL']))
+    print('{:-^170}'.format('-'))
