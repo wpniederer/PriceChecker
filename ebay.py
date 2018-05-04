9829a893ccc39253a2108f5bee1fe6f9b259cff6
Walt <wpniederer@cpp.edu>
2018-05-03 23:34:51 -0700

    created function for print/search

--- ebay.py
+++ ebay.py
@@ -1,41 +1,55 @@
-# give link for buying the album
-# listing title -- price -- url
-import datetime
 import config
-import time
 from ebaysdk.finding import Connection as Finding
 
-
 app_id = config.app_id
-search = 'The Strokes'
 max_price = '20'
 min_price = '5'
 condition = 'New'
 num_to_print = '50'
 
 
-try:
-    ebay_client = Finding(siteid='EBAY-US', appid=app_id, config_file=None)
-    ebay_client.execute('findItemsAdvanced', {
-        'keywords': search,
-        'categoryId': 176985,
-        'itemFilter': [
-            {'name': 'MaxPrice', 'value': max_price, 'paramName': 'Currency', 'paramValue': 'USD'},
-            {'name': 'MinPrice', 'value': min_price, 'paramName': 'Currency', 'paramValue': 'USD'},
-            {'name': 'AuctionWithBIN'},
-            {'name': 'Condition', 'value': condition},
-            {'name': 'HideDuplicateItems', 'value': 'true'}
-        ],
-        'paginationInput': {
-            'entriesPerPage': num_to_print,
-            'pageNumber': '1'
-        },
-        'sortOrder': 'CurrentPriceHighest'
-    })
-except ConnectionError as e:
-    print(e)
+def printResults(search, search_results):
+    print('\n{:=^170}'.format('Search results for ' + search))
 
-search_results = ebay_client.response.dict()
+    for item in search_results['searchResult']['item']:
+        print('Title: {:70}'.format(item['title']))
+        print('Price: ${:8}'.format(item['sellingStatus']['currentPrice']['value']))
+        print('URL: {:90}'.format(item['viewItemURL']))
+        print('{:-^170}'.format('-'))
+
+
+def ebaySearch(search):
+    try:
+        ebay_client = Finding(siteid='EBAY-US', appid=app_id, config_file=None)
+        ebay_client.execute('findItemsAdvanced', {
+            'keywords': search,
+            'categoryId': 176985,
+            'itemFilter': [
+                {'name': 'MaxPrice', 'value': max_price, 'paramName': 'Currency', 'paramValue': 'USD'},
+                {'name': 'MinPrice', 'value': min_price, 'paramName': 'Currency', 'paramValue': 'USD'},
+                {'name': 'AuctionWithBIN'},
+                {'name': 'Condition', 'value': condition},
+                {'name': 'HideDuplicateItems', 'value': 'true'}
+            ],
+            'paginationInput': {
+                'entriesPerPage': num_to_print,
+                'pageNumber': '1'
+            },
+            'sortOrder': 'CurrentPriceHighest'
+        })
+    except ConnectionError as e:
+        print(e)
+
+    search_results = ebay_client.response.dict()
+    return search_results
+
+
+
+#search_results = ebaySearch(search)
+#printResults(search, search_results)
+
+
+###search_results = ebay_client.response.dict()
 ###Need to figure out how to prevent searches with no results#####
 #if(len(search_results['searchResult']) == 0):
 #if(search_results['searchResult']):
@@ -44,12 +58,17 @@ search_results = ebay_client.response.dict()
     #quit()
 
 #else:
-print('\n{:=^170}'.format('Search results for ' + search))
 #print('{:^8}|{:^70}|{:^90}'.format('Price', 'Title','URL'))
 #print('{:=^170}'.format('='))
 
-for item in search_results['searchResult']['item']:
-    print('Title: {:70}'.format(item['title']))
-    print('Price: ${:8}'.format(item['sellingStatus']['currentPrice']['value']))
-    print('URL: {:90}'.format(item['viewItemURL']))
-    print('{:-^170}'.format('-'))
+
+if __name__ == "__main__":
+    search = input('Search for: ')
+    search_results = ebaySearch(search)
+    printResults(search, search_results)
+else:
+    import sys
+
+    search = str(sys.argv[1])
+    search_results = ebaySearch(search)
+    printResults(search, search_results)
