d6a668976bd4dceae74180b6db758657756e58d8
Walt <wpniederer@cpp.edu>
2018-05-04 23:32:55 -0700

    working on parsing input

--- main.py
+++ main.py
@@ -22,13 +22,22 @@ def getInput():
         one_line = input()
         if (one_line and one_line != '.eot'):
             user_input.append(one_line)
+        # um excuse u
         if (one_line == '.eot'):
             break
     return user_input
 
 def parseInput(input, line_n):
     if (len(input[line_n]) > 0):
-        line = input[line_n].split(' ')
+        line = input[line_n]
+        #print(line)
+        query = line.split('--')
+        query_length = len(query[0])
+        #query.  line[1:].split(' ')
+        line = line[query_length:].split(' ')
+        line.insert(0, query[0].strip(' '))
+        #print(query[0])
+        #print(line)
         return line
     else:
         print('Invalid line')
@@ -45,6 +54,12 @@ def getSwitches(line):
     else:
         print("Invalid switch or invalid format for input")
 
+def getswitchModifier(line, switch):
+    modifer_index = line.index(switch)
+    return int(line[modifer_index + 1])
+
+
+
 def ebaySwitches(line):
     print()
 
@@ -52,13 +67,16 @@ def ebaySwitches(line):
 def discogSwitches(line):
     query = line[0]
     num_to_print = 50
+    print(line)
     for switch in line:
         if (switch == '-n'):
-            num_to_print = 5
+            num_to_print = getswitchModifier(line, '-n')
+            print(num_to_print)
         if (switch == '-a'):
             discogs.discogsArtistSearch(query)
         if (switch == '-r'):
             discogs.discogsRecordSearch(query.split('|'), num_to_print)
+
 
 def twitterSwitches(line):
     print()
@@ -69,8 +87,8 @@ def redditSwitches(line):
 user_input = getInput()
 if (len(user_input) == 1):
     search_from = parseInput(user_input, 0)
-    print(search_from)
-    #getSwitches(search_from)
+    #print(search_from)
+    getSwitches(search_from)
 
 elif (len(user_input) == 2):
     search_from = parseInput(user_input, 0)
